1b8cb94e005c14d0bd9a2669e8242cfc962ab547
cecylia <cecylia.borek@gmail.com>
2020-05-22 10:33:15 +0200

    PoC version 2, .gitignore added

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -3,18 +3,33 @@ import numpy as np
 N = 10
 simulation_rounds  = 1000
 
-successful = 0
-collisions = 0
+successful = np.zeros(N)
+collisions = np.zeros(N)
+cw = np.ones(N) * 16
+
+backoffs = np.random.randint(low=0, high=16, size=N)
+
 
 for round in range(simulation_rounds):
-    backoffs = np.random.randint(low=0, high=16, size=N)
-    next_tx = np.where(backoffs == np.amin(backoffs))[0]
+    min_backoff = np.amin(backoffs)
+    next_tx = np.where(backoffs == min_backoff)[0]
+    backoffs -= min_backoff
     if len(next_tx) == 1:
-        successful +=1
+        successful[next_tx] +=1
+        cw[next_tx] = 16
+        backoffs[next_tx] = np.random.randint(low=0, high=cw[next_tx])
     else:
-        collisions +=1
+        for tx in next_tx:
+            collisions[tx] +=1
+            if cw[tx] < 1024:
+                cw[tx] *= 2
+            backoffs[tx] = np.random.randint(low=0, high=cw[tx])
 
-transmissions = np.array([successful, collisions])
 
-print(transmissions)
-print(transmissions/simulation_rounds * 100)
+collision_probability = np.array([collisions[sta]/(successful[sta] + collisions[sta]) for sta in range(N)])
+mean_collision_probability = np.mean(collision_probability)
+
+print("per STA successful transmissions:", successful)
+print("per STA collisions:", collisions)
+print("per STA collision probability:", collision_probability)
+print("average collision probability:", mean_collision_probability)
