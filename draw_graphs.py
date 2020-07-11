f58a2acb4b9d22dc110080f12581e7967702de1a
cecylia <cecylia.borek@gmail.com>
2020-07-11 15:43:33 +0200

    adds graphical improvements to graphs
    * sentence case in labels and legends
    * ns3 -> ns-3
    * deletion of mean from titles
    * connecting points with dashed line
    * graphs/simulation_result_15_1023_retry.png moved to graphs/old

--- draw_graphs.py
+++ draw_graphs.py
@@ -8,7 +8,6 @@ cw_min = 15
 cw_max = 1023
 N = 10 #number of contending stations
 retry_limit = True
-no_stations = [i + 1 for i in range(N)]
 
 if retry_limit:
     retry = 'retry'
@@ -21,32 +20,31 @@ final_results = pd.read_csv(f'results/final_results_{cw_min}_{cw_max}_{retry}.cs
 MSE_model = np.square(np.subtract(final_results['p_coll_simulation'], final_results['p_coll_model'])).mean()
 MSE_matlab = np.square(np.subtract(final_results['p_coll_simulation'], final_results['p_coll_matlab'])).mean()
 MSE_ns3 = np.square(np.subtract(final_results['p_coll_simulation'], final_results['p_coll_ns3'])).mean()
-print(MSE_model)
-print(MSE_matlab)
 
 plt.figure()
 plt.plot(
-    no_stations,
+    final_results['N'],
     final_results['p_coll_simulation'],
-    'bo',
-    no_stations,
+    'bo--',
+    final_results['N'],
     final_results['p_coll_model'],
-    'ro',
-    no_stations,
+    'ro--',
+    final_results['N'],
     final_results['p_coll_matlab'],
-    'yo',
-    no_stations,
+    'yo--',
+    final_results['N'],
     final_results['p_coll_ns3'],
-    'co'
+    'co--'
     )
 
-plt.title(f"mean probability of collision for cwmin={cw_min} and cwmax={cw_max} \n {retry}")
-plt.xlabel('number of contending stations')
-plt.ylabel('mean probability of collision')
-plt.legend(['simulation', 'model', 'matlab simulation', 'ns3'])
-plt.figtext(0.6, 0.25, 'MSE model: %.4E' % Decimal(MSE_model))
-plt.figtext(0.6, 0.2, 'MSE matlab: %.4E' % Decimal(MSE_matlab))
-plt.figtext(0.6, 0.15, 'MSE ns3: %.4E' % Decimal(MSE_ns3))
+plt.title(f"Mean probability of collision for cwmin={cw_min} and cwmax={cw_max} \n {retry}")
+plt.xlabel('Number of contending stations')
+plt.ylabel('Probability of collision')
+plt.legend(['DCF-NumPy', 'Analytical model', 'Matlab simulation', 'ns-3'])
+plt.figtext(0.55, 0.3, 'MSE:')
+plt.figtext(0.55, 0.25, '* Analytical model: %.4E' % Decimal(MSE_model))
+plt.figtext(0.55, 0.2, '* Matlab: %.4E' % Decimal(MSE_matlab))
+plt.figtext(0.55, 0.15, '* Ns-3: %.4E' % Decimal(MSE_ns3))
 plt.savefig(f'results/graphs/p_coll_result_{cw_min}_{cw_max}_{retry}.png')
 
 #throughput graph
@@ -55,16 +53,16 @@ MSE_ns3_thr = np.square(np.subtract(final_results['throughput_simulation'], fina
 
 plt.figure()
 plt.plot(
-    no_stations,
+    final_results['N'],
     final_results['throughput_simulation'],
-    'bo',
-    no_stations,
+    'bo--',
+    final_results['N'],
     final_results['thr_ns3'],
-    'ro'
+    'ro--'
     )
-plt.title(f"throughput per station for cwmin={cw_min} and cwmax={cw_max} \n {retry}")
-plt.xlabel('number of contending stations')
-plt.ylabel('throughput [Mb/s]')
-plt.legend(['simulation', 'ns3'])
-plt.figtext(0.6, 0.5, 'MSE ns3: %.4E' % Decimal(MSE_ns3_thr))
+plt.title(f"Throughput per station for cwmin={cw_min} and cwmax={cw_max} \n {retry}")
+plt.xlabel('Number of contending stations')
+plt.ylabel('Throughput [Mb/s]')
+plt.legend(['DCF-NumPy', 'ns-3'])
+plt.figtext(0.15, 0.15, 'MSE: %.4E' % Decimal(MSE_ns3_thr))
 plt.savefig(f'results/graphs/throughput_result_{cw_min}_{cw_max}_{retry}.png')
