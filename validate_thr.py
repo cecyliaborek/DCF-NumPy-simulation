894102a1bc1d1d678dc4da8050f40886012e1346
cecylia <cecylia.borek@gmail.com>
2020-08-02 15:18:30 +0200

    adds editorial corrections to thr comparison graph

--- validate_thr.py
+++ validate_thr.py
@@ -28,24 +28,24 @@ ns3_thr = float(results[3])
 # matlab IEEE_802_11a.m simulator
 matlab_thr = 34.601371204701266
 
-results = (dcf_numpy_thr, ns3_thr, matlab_thr)
+results = (matlab_thr, dcf_numpy_thr, ns3_thr)
 
 # throughput bar graph
 y_pos = np.arange(len(results))
-labels = ('Dcf NumPy', 'Ns-3', 'Matlab 802_11_a.m')
+labels = ('Matlab-Słowik', 'Dcf-NumPy', 'ns-3')
 
 fig, ax = plt.subplots()
 
 rects = plt.bar(y_pos, results, align='center')
 plt.xticks(y_pos, labels)
-plt.ylabel('Throughput [Mbps]')
+plt.ylabel('Throughput [Mb/s]')
 plt.title(f'Throuhput comparison for CWmin={cw_min}, CWmax={cw_max}, N={N}')
 
 # dispalying values for each bar
-for rect, value in zip(rects, results):
-    height = rect.get_height()
-    ax.text(rect.get_x() + rect.get_width()/2., height,
-            '%.4f' % value,
-            ha='center', va='bottom')
+#for rect, value in zip(rects, results):
+#    height = rect.get_height()
+#    ax.text(rect.get_x() + rect.get_width()/2., height,
+#            '%.4f' % value,
+#            ha='center', va='bottom')
 
 plt.savefig('results/graphs/thr_comparision.png')
