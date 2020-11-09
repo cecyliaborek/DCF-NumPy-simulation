2df8ee64d05456135c59ac2ccada03d5dbdb29af
cecylia <cecylia.borek@gmail.com>
2020-11-09 12:14:22 +0100

    runs ns-3 10 times for each payload value in thr vs payload comparison

--- results/thr_vs_payload/run_ns3.py
+++ results/thr_vs_payload/run_ns3.py
@@ -13,25 +13,29 @@ runs = [i + 1 for i in range(R)]
 mac_paylaods = [i for i in range(100, 2304, 100)]
 
 
-
 ns3_results = []
 
-for payload in mac_paylaods:
-    print('ns3:', payload)
-    stream = subprocess.check_output(
-        f'cd ~/ns-allinone-3.31/ns-3.31/ && ./waf --run "scratch/80211a-performance --simulationTime=100 --nWifi={N} --payload={payload}" | tail -n 2',
-        shell=True
-    )
+for r in runs:
+    for payload in mac_paylaods:
+        print(r, payload)
+        stream = subprocess.check_output(
+            f'cd ~/ns-allinone-3.31/ns-3.31/ && ./waf --run "scratch/80211a-performance --simulationTime=100 --nWifi={N} --payload={payload}" | tail -n 2',
+            shell=True
+        )
 
-    results = stream.decode('utf-8').strip().split()
-    thr = float(results[3])
+        results = stream.decode('utf-8').strip().split()
+        thr = float(results[3])
 
-    current_run = [payload, thr]
-    ns3_results.append(current_run)
+        current_run = [r, payload, thr]
+        ns3_results.append(current_run)
 
 ns3_results_dt = pd.DataFrame(ns3_results, columns=[
+    'run',
     'payload',
     'thr_ns3'
 ])
+
+ns3_results_dt = ns3_results_dt.groupby(
+    'payload')['thr_ns3'].mean().reset_index(name='thr_ns3')
 
 ns3_results_dt.to_csv('results/thr_vs_payload/ns3.csv')
