3215bd285f3528159710ee43a40dfd3b61121dc5
cecylia <cecylia.borek@gmail.com>
2020-11-28 11:32:53 +0100

    changes conf_intervals to be more universal

--- helpers/conf_intervals.py
+++ helpers/conf_intervals.py
@@ -2,8 +2,8 @@ import scipy.stats as st
 import numpy as np
 
 
-def confidence_intervals(results, field, alpha=0.05):
-    std = results.groupby('N').std().loc[:, field]
-    n = results.groupby('N').count().loc[:, field]
+def confidence_intervals(results, field, key, alpha=0.05):
+    std = results.groupby(key).std().loc[:, field]
+    n = results.groupby(key).count().loc[:, field]
     yerr = std / np.sqrt(n) * st.t.ppf(1-alpha/2, n - 1)
     return yerr
